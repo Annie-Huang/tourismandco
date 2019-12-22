d4460615e6014e9de665bde2d89f69482d7fe886
Annie Huang <annie.hn@gmail.com>
2019-12-22 23:04:31 +1100

    Change the class name as well.

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
