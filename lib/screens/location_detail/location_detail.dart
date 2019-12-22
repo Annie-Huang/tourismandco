99577e877055be1bd74dcf61cb7076197f9f9db5
Annie Huang <annie.hn@gmail.com>
2019-12-23 10:45:46 +1100

    Create 3 container as a Column children for body

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -9,7 +9,28 @@ class LocationDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text('Hello'),
       ),
-      body: Text(''),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            child: Text('hi'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Text('hi'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('hi'),
+          )
+        ]
+      ),
     );
   }
 }
