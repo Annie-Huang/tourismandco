efb9262a828f33c641f61659ebe190464e266eb2
Annie Huang <annie.hn@gmail.com>
2019-12-23 11:57:44 +1100

    Create theme property in app.dart so we can globally set the css.

--- lib/app.dart
+++ lib/app.dart
@@ -2,12 +2,18 @@
 
 import 'package:flutter/material.dart';
 import 'screens/location_detail/location_detail.dart';
+import 'style.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LocationDetail(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(title: AppBarTextStyle),
+        )
+      ),
     );
   }
 }
