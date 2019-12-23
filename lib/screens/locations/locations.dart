330627ade139c9f5ac0e99672d9ea32f123739d7
Annie Huang <annie.hn@gmail.com>
2019-12-23 14:14:27 +1100

    Wrapped the location.name into a GestureDetector so we can code specific an anonymous call back when tap is detected.

--- lib/screens/locations/locations.dart
+++ lib/screens/locations/locations.dart
@@ -8,12 +8,20 @@ class Locations extends StatelessWidget {
     // NOTE: we'll be moving this to a scoped_model later
     final locations = Location.fetchAll();
 
+    // GestureDetector class
+    // A widget that detects gestures.
+    // Attempts to recognize gestures that correspond to its non-null callbacks.
     return Scaffold(
       appBar: AppBar(
         title: Text('Locations'),
       ),
       body: ListView(
-        children: locations.map((location) => Text(location.name)).toList(),
+        children: locations
+            .map((location) => GestureDetector(
+              child: Text(location.name),
+              onTap: () => _onLocationTap(context, location.id),
+            ))
+            .toList(),
       ),
     );
   }
