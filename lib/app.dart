7932a307ad19227175be235afd4062320e582799
Annie Huang <annie.hn@gmail.com>
2019-12-23 15:16:18 +1100

    Create a RouteFactory in app.dart. The syntax is getting more confusing.

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'screens/locations/locations.dart';
+import 'screens/location_detail/location_detail.dart';
 import 'style.dart';
 
 const LocationsRoute = '/';
@@ -11,7 +12,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Locations(),
+//      home: Locations(),
+      onGenerateRoute: _routes(),
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           textTheme: TextTheme(title: AppBarTextStyle),
@@ -22,5 +24,23 @@ class App extends StatelessWidget {
         )
       ),
     );
+  }
+
+  RouteFactory _routes() {
+    return (settings) {
+      final Map<String, dynamic> arguments = settings.arguments;
+      Widget screen;
+      switch (settings.name) {
+        case LocationsRoute:
+          screen = Locations();
+          break;
+        case LocationDetailRoute:
+          screen = LocationDetail(arguments['id']);
+          break;
+        default:
+          return null;
+      }
+      return MaterialPageRoute(builder: (BuildContext context) => screen);
+    };
   }
 }
