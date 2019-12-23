ef6015a2ce88619bb31be42be002c6f6b61ecb61
Annie Huang <annie.hn@gmail.com>
2019-12-23 11:02:31 +1100

    Pass the color into the TextSection

--- lib/screens/location_detail/text_section.dart
+++ lib/screens/location_detail/text_section.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TextSection extends StatelessWidget {
+  final Color _color;
+
+//  TextSection(Color color) {
+//    this._color = color;
+//  }
+  // The below is is shorthand of above
+  TextSection(this._color);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.red,
+        color: _color,
       ),
       child: Text('hi'),
     );
