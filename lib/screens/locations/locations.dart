72eb8383d90f9c7a818462b16e1a384ffaa00a3b
Annie Huang <annie.hn@gmail.com>
2019-12-23 13:56:03 +1100

    Create the basic structure for locations.dart

---
+++ lib/screens/locations/locations.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../../app.dart';
+import '../../models/location.dart';
+
+class Locations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
