fb48328b3b154458684ad825399ded8de59d0bae
Annie Huang <annie.hn@gmail.com>
2019-12-23 14:59:07 +1100

    In location_detail.dart, instead of featchAll and show the first location, we will make call to fetchByID for the location.

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -7,10 +7,17 @@ import 'text_section.dart';
 import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
+  final int _locationID;
+
+  LocationDetail(this._locationID);
+
   @override
   Widget build(BuildContext context) {
-    final locations = Location.fetchAll();
-    final location = locations.first;
+//    final locations = Location.fetchAll();
+//    final location = locations.first;
+
+    // NOTE: we'll be moving this to a scoped_model later
+    final location = Location.fetchByID(_locationID);
 
     return Scaffold(
       appBar: AppBar(
